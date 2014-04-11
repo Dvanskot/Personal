5f918752eb8e6bd0a776cfeb6d30546234002652
Dvanskot <dvanskot@gmail.com>
2014-04-11 14:45:16 +0200

    account.php

---
+++ admin.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+   
+
+    <title>Smart Reading</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/design.css" rel="stylesheet">
+
+      <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+      <div class="container"> 
+      <div class="dyn-header">
+            <div class="inner">
+              <h3 class="masthead-brand"></h3>
+              <ul class="nav masthead-nav">
+                <li class="active">
+                <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="catalogue.php">Catalogue</a></li>
+                    <li><a href="search.php">Search</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="register.php">Register</a></li>                    
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="cart.php">Shopping Cart (0)</a></li>
+              </ul>
+            </div>
+          </div><br/>
+       <div class="row">
+            
+           <div class="col-xs-6 col-md-12">
+               Carousel
+           </div>
+           
+       </div>
+          <div class="page-header">
+<p>
+</p>
+<h1>Welcome To The Admin</h1></div>
+      <div class="row">
+        
+          <div class="col-xs-6 col-md-3">
+        
+          <div class="bs-sidenav">
+    		<ul class="nav nav-list bs-docs-sidenav affix-top">
+              <li><a href="#"><i class="icon-chevron-right"></i>Upload</a></li>
+              <li><a href="#"><i class="icon-chevron-right"></i>View Transaction</a></li>
+              <li><a href="#"><i class="icon-chevron-right"></i>View Users</a></li>              
+              
+            </ul>
+        </div>
+         
+        </div>
+          <div class="col-xs-6 col-md-9">
+                
+                </div>
+              </div>
+                
+          <br/>
+          
+       <div class="footer">
+              Designed &copy;2014. By <a href="http://smartreading.co.za">Smart Reading</a>
+            </div>
+         
+          </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
