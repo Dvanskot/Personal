be30de77d7963c6f279267b1f8be273974f8f172
Dvanskot <dvanskot@gmail.com>
2014-05-10 10:35:56 +0200

    Update firstserve.sql

--- firstserve.sql
+++ firstserve.sql
@@ -167,7 +167,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`username`, `email`, `password`, `country`, `act_code`, `is_active`, `is_ban`, `is_admin`, `validation`, `date_created`, `date_lastlogin`, `cellnum`, `postalAddr`, `fullname`) VALUES
-('lancelot@gmail.com', 'Lance@gmail.com', '71e8afeb3f980ecc4d4b9d886e3a543c', 'South Africa', 7960, 1, 0, 0, '0451bfaf87ea00b20588bfadc69e8323', '2014-05-08', '2014-03-04', '0810747099', 'P.O.Box 1423\r\nMakhado\r\n0960', 'Lancelot Fhatuwani Tshikota'),
+('lance@gmail.com', 'Lance@gmail.com', '71e8afeb3f980ecc4d4b9d886e3a543c', 'South Africa', 7960, 1, 0, 0, '0451bfaf87ea00b20588bfadc69e8323', '2014-05-08', '2014-03-04', '0810747099', 'P.O.Box 1423\r\nMakhado\r\n0960', 'Lancelot Fhatuwani Tshikota'),
 ('kg@gmail.com', 'kg@gmail.com', 'KG123t', 'South Africa', 1962, 1, 0, 0, '0451bfaf87ea00b20588bfadc69e8323', '2014-05-03', '', '0810747099', '124 The Ochards, Pretoria North 0052', 'Lancelot Tshikota');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
